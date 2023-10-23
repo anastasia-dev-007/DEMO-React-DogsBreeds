de96bcff87d216965a460340aac46ae7ddd6f6e3
anastasia-dev-007 <133599958+anastasia-dev-007@users.noreply.github.com>
2023-10-23 17:19:30 +0300

    commit

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
 import Breed from "../components/Breed";
 import Footer from "../components/Footer";
 
