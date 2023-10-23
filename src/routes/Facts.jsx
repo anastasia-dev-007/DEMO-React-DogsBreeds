7d874dbd45186a2fdf5c4b56d4a08524a16926ef
anastasia-dev-007 <133599958+anastasia-dev-007@users.noreply.github.com>
2023-10-23 15:45:14 +0300

    Facts

--- src/routes/Facts.jsx
+++ src/routes/Facts.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
+
 function Facts() {
+  const [facts, setFacts] = useState({});
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    const fetchFacts = async () => {
+      const response = await fetch('https://dogapi.dog/api/v2/facts?limit=5');
+      const data = await response.json();
+
+      setFacts(data);
+    };
+
+    fetchFacts();
+  }, []);
+
+//vom filtra array facts si il vom salva intr-o const noua pe care o vom utiliza in return facts
+
   return (
-    <div>Facts page</div>
+    <>
+      <input type="text" placeholder="Search..." onChange={(event) => setInputValue(event.target.value)} />
+      {facts.data?.filter(item => item.attributes.body.includes(inputValue)).map(item => (
+        <p key={item.id}>{item.attributes.body}</p>
+      ))}
+    </>
   );
 };
 
